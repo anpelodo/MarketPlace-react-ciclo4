1816441c0994e2eb39fb9ab5cbb7c473e2aec497
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-11-16 13:38:41 -0500

    Se agrega el modulo home

---
+++ src/modules/home/components/HomeHeader.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Link, useRouteMatch } from "react-router-dom";
+
+export default function HomeHeader() {
+  let { path } = useRouteMatch();
+
+  return (
+    <header>
+      <nav style={{ display: "flex", alignItems: "center" }}>
+        <h1>Marketplace React</h1>
+        <Link style={{ paddingLeft: "5px" }} to={`${path}`}>
+          Store
+        </Link>
+        <Link style={{ paddingLeft: "5px" }} to={`${path}/cart`}>
+          Carrito
+        </Link>
+        <Link style={{ paddingLeft: "5px" }} to={`${path}/product`}>
+          Product
+        </Link>
+        <Link style={{ paddingLeft: "5px" }} to={`/auth`}>
+          Login
+        </Link>
+        <Link style={{ paddingLeft: "5px" }} to={`/admin`}>
+          Admin
+        </Link>
+      </nav>
+    </header>
+  );
+}
---
+++ src/modules/home/views/StoreView.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StoreView() {
+  return (
+    <div>
+      <h3>Store View</h3>
+    </div>
+  );
+}
