83e2a84f9010eb6eb524f958929b896b64d8dacb
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-11-16 13:39:06 -0500

    Se integra todo en el componente App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React from "react";
 import "./App.scss";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import HomeView from "./modules/home/HomeView";
+import AuthView from "./modules/auth/AuthView";
+import AdminView from "./modules/admin/AdminView";
+import NoMatch from "./modules/common/NoMatch";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo - React</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+
+        <Route path="/home">
+          <HomeView />
+        </Route>
+
+        <Route path="/auth">
+          <AuthView />
+        </Route>
+
+        <Route path="/admin">
+          <AdminView />
+        </Route>
+
+        <Route path="/404-not-found">
+          <NoMatch />
+        </Route>
+
+        <Route path="*">
+          <Redirect to="/404-not-found" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
