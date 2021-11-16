308463bcdfdb859cb894c4ebb89324acd830f97c
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-11-16 13:37:07 -0500

    Se agrega el módulo admin

---
+++ src/modules/admin/AdminView.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AdminHeader from "./components/AdminHeader";
+
+export default function AdminView() {
+  return (
+    <div>
+      <AdminHeader />
+      <h2>Admin View</h2>
+    </div>
+  );
+}
---
+++ src/modules/admin/components/AdminHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default function AdminHeader() {
+  return (
+    <header>
+      <nav style={{ display: "flex", alignItems: "center" }}>
+        <h1>Marketplace Admin product</h1>
+        <Link style={{ paddingLeft: "5px" }} to="/">
+          Store
+        </Link>
+      </nav>
+    </header>
+  );
+}
