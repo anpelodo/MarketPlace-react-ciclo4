4f8a11f81c2587fd9283991a25e5d134b0618d99
Juan Esteban Londoño <jestebanlondono11@gmail.com>
2021-11-21 22:40:21 -0500

    Avance maquetacion vista admin

--- src/modules/admin/AdminView.js
+++ src/modules/admin/AdminView.js
@@ -1,11 +1,47 @@
 import React from "react";
 import AdminHeader from "./components/AdminHeader";
+import Table from 'react-bootstrap/Table';
+// import Button from "@restart/ui/esm/Button";
+
+import  ModalEdit from './components/ModalEdit';
+import ModalAdd from './components/ModalAdd';
+import ModalDelete from './components/ModalDelete';
+
 
 export default function AdminView() {
   return (
     <div>
-      <AdminHeader />
-      <h2>Admin View</h2>
+      <div className="container">
+        <AdminHeader />
+        <h2>Admin View</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Codigo</th>
+              <th>Producto</th>
+              <th>Categoria</th>
+              <th>Stock</th>
+              <th>Precio</th>
+              <th>  </th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td><ModalEdit  /></td>
+              <td><ModalDelete /></td>
+            </tr>
+          </tbody>
+        </Table>
+        <ModalAdd />
+        
+       
+      </div>
     </div>
   );
 }
