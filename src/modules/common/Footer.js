0898fd97f315966524583a9d1b7eeaa8ef325f95
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-11-26 16:20:06 -0500

    Correccion en el footer

--- src/modules/common/Footer.js
+++ src/modules/common/Footer.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="d-flex flex-column flex-sm-row align-items-center align-items-sm-start justify-content-evenly bg-light p-3">
+    <footer className="d-flex flex-column flex-sm-row align-items-center align-items-sm-start justify-content-evenly bg-light p-3 w-100">
       <div className="d-flex flex-column">
         <h2 className="fw-bold text-primary">Contacto</h2>
 
-        <div className="text-info">
-          <p>
+        <div className="text-info mb-2">
+          <div className="mb-2">
             <div> Persona 1 </div>
             <div> email: persona1@gmail.com </div>
-          </p>
-          <p>
+          </div>
+          <div>
             <div> Persona 2 </div>
             <div> email: persona2@gmail.com </div>
-          </p>
+          </div>
         </div>
       </div>
 
