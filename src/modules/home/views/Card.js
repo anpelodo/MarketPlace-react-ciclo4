c852456d2d073c7d4fb5b84a3c2e3ae1a13cca51
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-12-13 14:12:17 -0500

    Cambios visuales a la tarjeta de producto

--- src/modules/home/views/Card.js
+++ src/modules/home/views/Card.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-export default function Card({title, imageUrl, descripcion, precio}) {
-    
+export default function Card({ title, imageUrl, precio }) {
   return (
-    <div className="card text-center"> 
-        <img src={imageUrl}/>
-        <div className="card-body">
-            <h4 className="card-tittle">{title}</h4>
-            <p className="card-text">{descripcion}</p>
-            <p className="card-text">{precio}</p>
-            <a href="" className="btn btn-outline-secondary rounded-0">Comprar</a>
-        </div>
+    <div
+      className="card d-flex flex-column"
+      style={{ minHeight: '26rem', width: '18rem' }}
+    >
+      <div
+        className="d-flex flex-wrap align-content-center justify-content-center"
+        style={{ height: '18rem', width: '17.8rem' }}
+      >
+        <img src={imageUrl} alt={title + ' image'} className="card-img-top" />
+      </div>
+      <div className="card-body">
+        <h4 className="card-tittle">{title}</h4>
+        <p className="card-text fw-bold text-primary fs-4">${precio}</p>
+      </div>
     </div>
   );
 }
--- src/modules/home/views/StoreView.js
+++ src/modules/home/views/StoreView.js
@@ -3,10 +3,9 @@ import Card from './Card';
 import axios from 'axios';
 import { useState } from 'react';
 
-const baseURL = "http://localhost:3002/api/product/list";
+const baseURL = 'http://localhost:3002/api/product/list';
 
 export default function StoreView() {
- 
   const [post, setPost] = useState([]);
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -22,19 +21,16 @@ export default function StoreView() {
       <h3>Store View</h3>
       <div className="container d-flex justify-content-center">
         <div className="row">
-          {
-            post.map(post =>(
-              <div className="col-md-4" key={post._id}>
-                <Card title={post.nombre} imageUrl={post.img} descripcion={post.descripcion} precio={post.precio}/>
-              </div>
-            ))
-
-
-          }
-          
-         
-         
-          
+          {post.map((post) => (
+            <div className="col-12 col-md-6 col-lg-3" key={post._id}>
+              <Card
+                title={post.nombre}
+                imageUrl={post.img}
+                descripcion={post.descripcion}
+                precio={post.precio}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
